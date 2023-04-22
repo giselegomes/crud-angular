87131d16e38cd3acf4eeb1d2e930596a7ab80d14
Gisele Gomes <gselegomes@gmail.com>
2023-04-21 21:01:20 -0300

    feat: adds module pessoa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PessoaModule } from './pessoa/pessoa.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PessoaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
